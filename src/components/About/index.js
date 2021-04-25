dd06d49bbf142eb0a26df2a00f50c703525adf63
Emmanuel Sobamowo <sobamowo.laolu@hotmail.com>
2021-04-25 14:09:23 +0100

    Add About section

---
+++ src/components/About/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <>
+        <div id="About-section">
+        <h1 id="About"> About Me... </h1>
+        <h4 id="Sub-About"> Welcome to my portfolio</h4>
+        <p>I am a graduate from the University of Liverpool, with a BA in Business Studies and Politics.
+            Following my graduation in 2019, I started work at GetAgent.co.uk as a Customer Service Manager. 
+            I recently made the decision to puruse a career in the technology industry, following months of online HTML and Javascript courses on Udemy. 
+            I was fortunate enough to begin my journey with Futureproof and attend the coding bootcamp, training as a full stack developer.
+            I have been able to grow as a full-stack developer, and specialise in React and Javascript. 
+            This is my portfolio with some of the projects I have started, and been a part of. 
+            This is just the start of my software development journey, and I am eager to log all my progress and ideas!</p>
+        </div>
+        </>
+    )
+}
+export default About;
--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,6 @@
+export {default as About} from './About'
+export {default as BackButton} from './BackButton'
 export {default as Header} from './Header'
+export {default as Navbar} from './Navbar'
+export {default as Projects} from './Projects'
+export {default as Skills} from './Skills'
