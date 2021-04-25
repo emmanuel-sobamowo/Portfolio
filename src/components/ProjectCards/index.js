f61a0fb0458231535989349d814ee6c5d82edd1a
Emmanuel Sobamowo <sobamowo.laolu@hotmail.com>
2021-04-25 16:19:11 +0100

    Add Project Cards

---
+++ src/components/ProjectCards/index.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from 'react';
+
+const ProjectCards = ({ projects }) => {
+
+
+  return (
+    <main className="project">
+      <div>
+        <h3>{projects.name}</h3>
+        <p className="projectDescription">{projects.description}</p>
+        <button className="visit-website"><a href={projects.netlify} target="_blank">Visit The Site  <i className="project-links"></i></a></button>
+        <button className="visit-repo"><a href={projects.github} target="_blank">Visit Sourcecode  <i className="project-links"></i></a></button>
+      </div>
+
+      <div>
+        <img src={projects.image} alt={projects.alt} className="project-img" />
+      </div>
+
+    </main >
+  )
+}
+
+export default ProjectCards;
