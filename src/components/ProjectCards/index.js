d891fb15244c02789e59f3e920246457207cdf81
Emmanuel Sobamowo <sobamowo.laolu@hotmail.com>
2021-04-25 22:31:59 +0100

    Fix buttons

--- src/components/ProjectCards/index.js
+++ src/components/ProjectCards/index.js
@@ -15,8 +15,8 @@ const ProjectCards = ({ repos }) => {
         <img src={repos.image} className="project-img" />
       </div>
       <div>
-        <button className="visit-website"><a href={repos.netlify} target="_blank">Visit The Site  <i className="project-links"></i></a></button>
-        <button className="visit-repo"><a href={repos.github} target="_blank">Visit Sourcecode  <i className="project-links"></i></a></button>
+        <button className="visit-website"><a id="link" href={repos.netlify} target="_blank">Visit The Site  <i className="project-links"></i></a></button>
+        <button className="visit-repo"><a id="link" href={repos.github} target="_blank">Visit Sourcecode  <i className="project-links"></i></a></button>
       </div>
 
     </main >
