9fbb7319bb4f605e99d5b335dbb643e1766b3e85
Emmanuel Sobamowo <sobamowo.laolu@hotmail.com>
2021-04-26 15:11:26 +0100

    Change the fonts and Skill cards

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -3,45 +3,31 @@ import './app.css';
 const Skills = () => {
     return (
         <>
-        <div class="skills-card" id="skills-section">
-            <div class="skills-container">
-                <h1>Skills</h1>
-                <h4>Below are a some of the skills I have developed from my work experience and my Futureproof Coding Bootcamp.</h4>
-
-                    <table id="skills">
-                    <tr>
-                        <th>Technical</th>
-                        <th>Team-Based</th>
-                    </tr>
-                    <tr>
-                        <td>HTML & CSS</td>
-                        <td>Effective Teamwork</td>
-                    </tr>
-                    <tr>
-                        <td>JavaScript & Node.js</td>
-                        <td>Clear Communication</td>
-                    </tr>
-                    <tr>
-                        <td>PostgreSQL & MongoDB</td>
-                        <td>Presenting</td>
-                    </tr>
-                    <tr>
-                        <td>PYTHON</td>
-                        <td>Active Listener</td>
-                    </tr>
-                    <tr>
-                        <td>RESTful API's</td>
-                        <td>Eager Learner</td>
-                    </tr>
-                    <tr>
-                        <td>Responsive Web-Design</td>
-                        <td>Organised and Perseverant</td>
-                    </tr>
-                    </table>
+        <div class="row" id="skills-section">
+            <div class="column">
+                <div class="skills-container">
+                    <h3>Some Of My Technical Skills...</h3>
+                    <p>HTML & CSS</p>
+                    <p>PostgreSQL & MongoDB</p>
+                    <p>JavaScript & Node.js</p>
+                    <p>PYTHON</p>
+                    <p>RESTful API's</p>
+                    <p>Responsive Web-Design</p>
+                </div>
+            </div>
+            <div class="column">
+                <div class="skills-container">
+                <h3>Some Of My Social Skills...</h3>
+                    <p>Effective Teamwork</p>
+                    <p>Clear Communication</p>
+                    <p>Active Listene</p>
+                    <p>Presenting</p>
+                    <p>Eager Learner</p>
+                    <p>Organised and Perseverant</p>
+                </div>
             </div>
         </div>
         </>
-
     )
 }
 
